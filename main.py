ab1bec7b84130ebde9feb670c6128f9b2996e096
Sevil Babayeva <sevaa_99@mail.ru>
2023-09-02 10:44:25 +0800

    add_sub

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from funcs import add_numbers
 result = add_numbers(3, 4)
 print(result)
+
+import funcs
+
+def subtract_numbers(x, y):
+    return x - y
+
+result2 = funcs.subtract_numbers(5, 7)
+print(result2)
